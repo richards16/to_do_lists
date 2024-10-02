678965cf8daac9af59172c8737888e2850fc1c57
richards16 <148983465+richards16@users.noreply.github.com>
2024-10-02 08:26:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const taskname = document.getElementById('task-name') ;
+const card_container=document.getElementById('card-contain');
+
+taskname.addEventListener("keypress",function(event){
+    const card=document.getElementById('card').cloneNode(true);
+    if(taskname.value == " " || taskname.value == ""){
+        window.alert("Enter a task");
+        return
+    }
+
+    if(event.key=="Enter"){
+        card.children[0].innerHTML=taskname.value ;
+        card_container.appendChild(card);
+        document.getElementById('task-name').value=''
+    }
+})
+
+function complete_task(button){
+    button.parentNode.parentNode.dataset.completed = "true";
+    sortTask()
+}
+
+function sortTask(){
+    
+    var completed = [];
+    var all_tasks=card_container.children;
+
+
+    for (let i = 0; i < all_tasks.length; i++) {
+
+        if (all_tasks[i].dataset.completed=="true") {
+
+            card_container.appendChild(all_tasks[i]
+
+            );
+            
+        }
+        
+    }
+}
